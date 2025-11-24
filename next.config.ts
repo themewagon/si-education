c4c717dfd85720584a8c9494e32c3a7479458cbc
utsaho <pipul.boddyo@gmail.com>
2025-11-24 15:54:42 +0600

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
 
-const basePath = '/si-education';
-// const basePath = process.env.NODE_ENV === 'production' ? '/si-education' : ''
+const basePath = '';
 
 const nextConfig: NextConfig = {
     /* config options here */
